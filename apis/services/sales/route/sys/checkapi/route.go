ddf459a8e27e0ad30e16f0db71d4a48babe9fb75
abram222 <abrammamdouh222@gmail.com>
2026-06-13 18:07:41 +0300

    panics middleware

--- apis/services/sales/route/sys/checkapi/route.go
+++ apis/services/sales/route/sys/checkapi/route.go
@@ -10,4 +10,6 @@ func Routes(app *web.App) {
 	app.HandleFunc("GET /readiness", readiness)
 	app.HandleFunc("GET /liveness", liveness)
 	app.HandleFunc("GET /testerror", testError)
+    app.HandleFunc("GET /testpanic", testPanic)  
+
 }
